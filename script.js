1cc5fb2e3ad73d9316eedee418d9537507fd142f
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-05 04:51:44 +0100

    changed map layout

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
